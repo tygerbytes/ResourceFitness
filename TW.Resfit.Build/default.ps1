844932c27a3b4856206c0920968b9986c18729d2
Ty Walls <tygerbytes@users.noreply.github.com>
2016-02-14 10:02:18 -0500

    Further flesh out psake skeleton

--- TW.Resfit.Build/default.ps1
+++ TW.Resfit.Build/default.ps1
@@ -1,24 +1,58 @@
 ﻿
-properties {
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
 
+properties {
+	$solutionDirectory = Split-Path -Parent $here
+	$outputDirectory = Join-Path $solutionDirectory ".build"
 }
 
 Task Default -depends BakeAndShake -description "Default task"
 
-Task BakeAndShake -depends Build, UnitTests, AcceptanceTests -description "Build solution and run all tests"
+Task BakeAndShake `
+	-description "Build solution and run all tests" `
+	-depends Build, UnitTests, AcceptanceTests 
 
-Task Clean -description "Clean up build cruft" {
+Task Clean `
+	-description "Clean up build cruft and initialize build folder structure" `
+	-requiredVariables $outputDirectory 
+{	
+	New-Directory $outputDirectory
+	Remove-Contents $outputDirectory
+}
+
+Task Build `
+	-description "Build them... Build them all." `
+	-depends Clean 
+{
 
 }
 
-Task Build -depends Clean -description "Build them... Build them all." {
-
-}
-
-Task UnitTests -depends Build -description "Run all unit tests" {
+Task UnitTests `
+	-description "Run all unit tests" `
+	-depends Build 
+{
 	
 }
 
-Task AcceptanceTests -depends Build -description "Run all acceptance tests" {
+Task AcceptanceTests `
+	-description "Run all acceptance tests" `
+	-depends Build 
+{
 
+}
+
+Function New-Directory {
+	[CmdletBinding()]
+	Param([string]$path)
+	Write-Output "Creating directory $path"
+	
+	New-Item -ItemType Directory $path -ErrorAction SilentlyContinue | Out-Null
+}
+
+Function Remove-Contents {
+	[CmdletBinding()]
+	Param([string]$path)
+	Write-Output "Removing contents of $path"
+
+	Get-ChildItem -Recurse -Path $path | Remove-Item -Force -Recurse -ErrorAction SilentlyContinue | Out-Null
 }
