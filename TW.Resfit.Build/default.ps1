a414d33fad6c34e4199d606947f535e6010dba9f
Ty Walls <tygerbytes@users.noreply.github.com>
2016-02-13 22:48:28 -0500

    Add Build project and Psake skeleton

---
+++ TW.Resfit.Build/default.ps1
@@ -0,0 +1,24 @@
+﻿
+properties {
+
+}
+
+Task Default -depends BakeAndShake -description "Default task"
+
+Task BakeAndShake -depends Build, UnitTests, AcceptanceTests -description "Build solution and run all tests"
+
+Task Clean -description "Clean up build cruft" {
+
+}
+
+Task Build -depends Clean -description "Build them... Build them all." {
+
+}
+
+Task UnitTests -depends Build -description "Run all unit tests" {
+	
+}
+
+Task AcceptanceTests -depends Build -description "Run all acceptance tests" {
+
+}
