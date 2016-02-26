fc984efd435017aa962d4bec659392687fd72932
Ty Walls <tygerbytes@users.noreply.github.com>
2016-02-26 06:51:53 -0500

    Setup PowerShell debugging and add first cmdlet

---
+++ TW.Resfit.Console/GetResource.cs
@@ -0,0 +1,16 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="GetResource.cs" company="Tygertec">
+//   Copyright © 2016 Tyrone Walls.
+//   All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace TW.Resfit.Console
+{
+    using System.Management.Automation;
+
+    [Cmdlet(VerbsCommon.Get, "Resource")]
+    public class GetResource : Cmdlet
+    {
+    }
+}
